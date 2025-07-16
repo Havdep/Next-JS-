1d8ba4df85b95ab364f739aa1af59d8d5694d61a
Nipun <nipunsingh29@hotmail.com>
2025-07-16 23:56:00 +0900

    Add loading.tsx

---
+++ src/app/blog/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white px-6 py-16">
+      Loading...
+    </div>
+  );
+};
+
+export default Loading;
